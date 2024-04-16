642c5bed9b2236d0f36f37f25f9f138ec0861b53
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-04-16 21:48:56 +0300

    Домашнее задание 2-12. Spring - начальный коммит

---
+++ src/main/java/pro/sky/homework212/JavaSkyProHomework212Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomework212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomework212Application.class, args);
+	}
+
+}
